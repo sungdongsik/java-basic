3600edca499f1465e4a322a2a63313cdc26676c1
성동식 <tjdehdtlr123@naver.com>
2025-01-13 00:20:32 +0900

    01-12 - final

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+    static final  int CONST_VALUE = 10;
+    final int value = 10;
+
+    // 초기값이 있을 경우 에러가 난다.
+    /*public FieldInit(int value){
+        this.value = value;
+    }*/
+}
